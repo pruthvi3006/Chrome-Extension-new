15297df8fce3f4502222581fde6b230de129eaa0
parthjoshi19 <devaccounts@decloud.org>
2025-06-27 12:42:48 +0530

    added skynet agents

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    background: './src/background.ts',
-    content: './src/content.tsx',
-    sidepanel: './src/sidepanel.tsx'
+    background: "./src/background.ts",
+    content: "./src/content.tsx",
+    sidepanel: "./src/sidepanel.tsx",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      fs: false,
+      crypto: false,
+      buffer: false,
+    },
+    alias: {
+      "@react-native-async-storage/async-storage": false,
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/sidepanel.html',
-      filename: 'sidepanel.html',
-      chunks: ['sidepanel']
-    })
-  ]
-}; 
+      template: "./src/sidepanel.html",
+      filename: "sidepanel.html",
+      chunks: ["sidepanel"],
+    }),
+  ],
+  externals: {
+    "@react-native-async-storage/async-storage": "undefined",
+  },
+};
